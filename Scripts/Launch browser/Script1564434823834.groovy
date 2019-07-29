3a4d1ec27d505b5dc679f75517a6d838fcd73f39
sk15853 <sk15853@kroger.com>
2019-07-29 18:35:22 -0400

    Hit to Hello

--- Scripts/Launch browser/Script1564434823834.groovy
+++ Scripts/Launch browser/Script1564434823834.groovy
@@ -14,4 +14,4 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser("Www.google.com")
-WebUI.comment("Hi")
+WebUI.comment("Hello")
